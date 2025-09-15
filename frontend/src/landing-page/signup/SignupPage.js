6d616b387640a93030bc9aee2feb8af32f7a2205
Khushboo30628 <khushbooabdullah86@gmail.com>
2025-09-15 18:54:54 +0530

    fixed

---
+++ frontend/src/landing-page/signup/SignupPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Signup from './Signup';
+
+function SignupPage() {
+    return (
+        <>
+            <Signup />
+
+        </>
+    );
+}
+
+export default SignupPage;  
